134c157938b317e7497c48b0d4604afe7aaf27a3
Artur <artur.kaluza@almservices.pl>
2018-08-03 13:23:19 +0200

    Get uesr worklogs by uesr key

--- client/src/components/Jira/Jira.js
+++ client/src/components/Jira/Jira.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './Jira.css';
-import { List, Segment } from 'semantic-ui-react';
+import { List, Segment, Form } from 'semantic-ui-react';
 
 
 class Jira extends Component {
@@ -10,17 +10,16 @@ class Jira extends Component {
 
     this.state = {
       projects: [],
-      activeTask: false
+      activeTask: false,
+      author: ''
     }
 
     this.checkActiveTask = this.checkActiveTask.bind(this);
-    this.getProjectList = this.getProjectList.bind(this);
+    this.getWorklogList = this.getWorklogList.bind(this);
   }
   
   componentDidMount() {
     this.checkActiveTask();
-    this.getProjectList();
-
   }
 
   componentWillReceiveProps(newProps) {
@@ -32,17 +31,28 @@ class Jira extends Component {
     
   }
 
-  // 
-  getProjectList() {
-    axios.get('/api/jira')
+  getWorklogList(e) {
+    e.preventDefault();
+    const key = e.target.key;
+    
+    axios.get(`/api/jira/${key.value}`)
       .then(res => {
-      const projectArray = []
+      this.setState({author: res.data.worklogs[0].author.name})
+      const projectArray = res.data.worklogs.map((item, index) => {
+        let obj = {}
+        
+        obj.comment = item.comment;
+        obj.id = item.id;
 
-      projectArray.push(res.data[2], res.data[8], res.data[9]);
+        return obj;
+      })
+     
       this.setState({projects: projectArray})
      
     })
     .catch(e => console.log(e));
+
+    key.value = ''
   }
    
 
@@ -52,16 +62,28 @@ class Jira extends Component {
         <div className='column__header'>
           <h2>Jira</h2>
           
+          <Form onSubmit={this.getWorklogList} style={{marginTop: '5px', display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+              <Form.Group>
+                <Form.Input placeholder="user key (almaca-41)" id='key' />
+                <Form.Button content='Submit' />
+              </Form.Group>
+          </Form>
+          
         </div>
         <Segment color='violet'>
+          <h3 className='user__title' >User name: {this.state.author}</h3>
           <List>
             {this.state.projects.map((item, index) => <List.Item
               className={this.state.activeTask === (index + 1) ? 'Jira__item list-item list-item__active' : 'list-item Jira__item'}
               key={index}
               onClick={() => this.props.jiraTask(index + 1)}
               > 
-              {item.name}
-
+                <div>
+                  {item.comment}
+                </div>
+                <div>
+                  <p>id: {item.id}</p>
+                </div>
             </List.Item>
             )}
           </List>
--- server/api/jira/jira.js
+++ server/api/jira/jira.js
@@ -3,17 +3,28 @@ const router = express.Router();
 const fetch = require('node-fetch');
 
 
-router.get('/', (req, res) => {
-  fetch('https://supportdesk.almservices.pl/rest/api/2/project/', {
+router.get('/:key', (req, res) => {
+  // fetch('https://supportdesk.almservices.pl/rest/api/2/project/', {
+  //   headers: {
+  //     "Authorization": 'Basic YXJ0dXIua2FsdXphOmFzZHp4YzEx',
+  //   }
+  // })
+  //   .then(data => data.json())
+  //   .then(datajosn => {
+  //     res.send(datajosn)
+  //   })
+  //   .catch(e => console.log(e));
+  
+  const { key } = req.params;
+
+  fetch(`https://supportdesk.almservices.pl/rest/api/2/issue/${key}/worklog`, {
     headers: {
       "Authorization": 'Basic YXJ0dXIua2FsdXphOmFzZHp4YzEx',
     }
-  })
-    .then(data => data.json())
-    .then(datajosn => {
-      res.send(datajosn)
-    })
-    .catch(e => console.log(e));
+  }).then(data => data.json())
+    .then(datajson => res.send(datajson))
+    .catch(e => console.log(e))
+
   
 });
 
