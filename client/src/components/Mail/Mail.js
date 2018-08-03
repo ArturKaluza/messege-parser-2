983a88feede3e85f0b767adbedefed76b621e957
Dilven <mateusiakkrystian@gmail.com>
2018-08-03 13:41:29 +0200

    bugs fix and change styles formails

--- client/src/components/Mail/Mail.js
+++ client/src/components/Mail/Mail.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import { Loader, List } from 'semantic-ui-react';
+import React, { Component, Fragment } from 'react'
+import { List, Segment } from 'semantic-ui-react';
 import axios from 'axios';
+import Loader from '../Loader/Loader'
 import MailThreads from '../MailThreads/MailThreads'
 
 export default class Mail extends Component {
@@ -33,18 +34,24 @@ export default class Mail extends Component {
   render() {
     const { threads } = this.state;
     return (
-      <div>
-        <Loader isLoading={this.state.isLoading} />
-        <List divided relaxed>
-        {threads.map(thread => {
-          return (
-            <MailThreads 
-              data={thread}
-            />
-          )
-        })}
-        </List>
-      </div>
+      <Fragment>
+        <div className='column__header'>
+          <h2>E-mail</h2>
+        </div>
+        <Segment color='orange'>
+          <Loader isLoading={this.state.isLoading} />
+          <List divided relaxed>
+          {threads.map((thread, index) => {
+            return (
+              <MailThreads 
+                data={thread}
+                key={index}
+              />
+            )
+          })}
+          </List>
+        </Segment>
+      </Fragment>
     )
   }
 }
